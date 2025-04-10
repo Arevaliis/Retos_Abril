65018973e8eb00d27fcea5b95259a3f5a83bfccf
Jose Iglesias <152866021+Arevaliis@users.noreply.github.com>
2025-04-10 12:44:42 +0200

    Update Reto_7.py

--- Retos Abril/Python/Reto_7.py
+++ Retos Abril/Python/Reto_7.py
@@ -1,9 +1,7 @@
 import random
 
 pasos = 20
-caracol_1 = 0
-caracol_2 = 0
-caracol_3 = 0
+partcipantes = int(input("Numero de participantes: "))
 
 # Lista para guardar la distancia final recorrida por cada caracol
 resultado = []
@@ -12,15 +10,11 @@ resultado = []
 ganador = ""
 
 # Simulamos la carrera: cada caracol avanza una cantidad aleatoria entre 1 y 6 por cada paso
-for i in range(pasos + 1):
-    caracol_1 += random.randint(1, 6)
-    caracol_2 += random.randint(1, 6)
-    caracol_3 += random.randint(1, 6)
-
-# Guardamos los resultados finales en la lista
-resultado.append(caracol_1)
-resultado.append(caracol_2)
-resultado.append(caracol_3)
+for participante in range(partcipantes):
+    caracol = 0
+    for i in range(pasos + 1):
+        caracol += random.randint(1, 6)
+    resultado.append(caracol)
 
 # Comprobamos si hay empate (es decir, más de un caracol con la máxima distancia dentro de la lista)
 if resultado.count(max(resultado)) > 1:
