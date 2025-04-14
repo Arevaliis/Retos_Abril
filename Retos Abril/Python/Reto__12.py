39901e115e516663b100460d063942976febf8af
Jose Iglesias <152866021+Arevaliis@users.noreply.github.com>
2025-04-14 11:54:23 +0200

    Add files via upload

---
+++ Retos Abril/Python/Reto__12.py
@@ -0,0 +1,42 @@
+estaFuncionando = True
+lista_numeros_suerte = []
+
+def desea_seguir():
+    """Función que pregunta al usuario si desea seguir ejecutando el programa"""
+    respuesta = input("¿Desea seguir?: (S/N)").upper().strip()
+    match respuesta:
+        case "S":  # Si el usuario responde "S", continuar
+            return True
+        case "N":  # Si responde "N", salir
+            print("Saliendo...")
+            return False
+        case _:  # Cualquier otra respuesta se considera inválida, y se vuelve a preguntar de forma recursiva
+            print("Responda S o N. ")
+            return desea_seguir()
+
+while estaFuncionando:
+    print("\n" * 2)
+    try:
+        numero = int(input("Introduce un numero: ").strip())
+
+        # Si el número es múltiplo de 7 y no ha sido introducido antes
+        if numero % 7 == 0 and str(numero) not in lista_numeros_suerte:
+            print(f"{numero} es un numero de la suerte!!")
+
+            # Añade el número a la lista de la suerte
+            if str(numero) not in lista_numeros_suerte:
+                lista_numeros_suerte.append(str(numero))
+
+            # Pregunta si desea seguir
+            estaFuncionando = desea_seguir()
+        else:
+            # Si no es múltiplo de 7 o ya fue introducido antes
+            print(f"{numero} no es un numero de la suerte o ya lo has dicho!!")
+            estaFuncionando = desea_seguir()
+
+    except ValueError:
+        # Captura errores si el usuario no introduce un número
+        print("Introduzca un Número")
+
+print(
+    f"Hemos obtenido un total de {len(lista_numeros_suerte)} números. Son los siguientes: {", ".join(lista_numeros_suerte)}.")
