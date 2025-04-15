feb51d4d6f063a27d174920199d6c0c062ad68a8
Jose Iglesias <152866021+Arevaliis@users.noreply.github.com>
2025-04-15 11:51:57 +0200

    Update Reto__15.py

--- Retos Abril/Python/Reto__15.py
+++ Retos Abril/Python/Reto__15.py
@@ -3,13 +3,11 @@ import re
 def valildar_contrasenya(contrasena):
     """Función que compara la contraseña con las restricciones para ver si es segura y valida"""
     patron = re.compile(
-                        r"^"                # Inicio de la Cadena
                         r"(?=.*[a-z])"      # Debe tener una minuscula
                         r"(?=.*[A-Z])"      # Debe contener una mayuscula
                         r"(?=.*[0-9])"      # Debe contener un numero
                         r"(?=.*[*?!^“$])"   # Debe contener un simbolo especial; *, ?, !, ^, “, $.
                         r".{8,}"            # Debe tener al menos 8 digitos
-                        r"$"                # Fin de la cadena
                         )
     return patron.match(contrasena) and 'git' not in contrasena.lower()
 
