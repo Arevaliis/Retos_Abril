8719e092cf4ae2b6b3a3477d9b973bd1fd6c63d3
Jose Iglesias <152866021+Arevaliis@users.noreply.github.com>
2025-04-15 11:46:33 +0200

    Add files via upload

---
+++ Retos Abril/Python/Reto__15.py
@@ -0,0 +1,29 @@
+import re
+
+def valildar_contrasenya(contrasena):
+    """Función que compara la contraseña con las restricciones para ver si es segura y valida"""
+    patron = re.compile(
+                        r"^"                # Inicio de la Cadena
+                        r"(?=.*[a-z])"      # Debe tener minuscula
+                        r"(?=.*[A-Z])"      # Debe contener Mayusculas
+                        r"(?=.*[0-9])"      # Debe contener numeros
+                        r"(?=.*[*?!^“$])"   # Debe contener un simbolo especial; *, ?, !, ^, “, $.
+                        r".{8,}"            # Debe tener al menos 8 digitos
+                        r"$"                # Fin de la cadena
+                        )
+    return patron.match(contrasena) and 'git' not in contrasena.lower()
+
+if valildar_contrasenya(input("Introduzca su contraseña: ")):
+    print("Contraseña segura y valida")
+else:
+    print("Esta contraseña no cumple con las reglas o no es segura. ")
+
+
+
+
+
+
+
+
+
+
