492e79080f6ac42f8c2d32f62a69c4153ee2d94a
Jose Iglesias <152866021+Arevaliis@users.noreply.github.com>
2025-04-25 10:14:50 +0200

    Add files via upload

---
+++ Retos Abril/Java/Reto23.java
@@ -0,0 +1,37 @@
+package RetosAbril;
+
+import java.util.Scanner;
+
+public class Reto23 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Dime una palabra: ");
+        String palabra1 = scanner.nextLine();
+        System.out.print("Dime una palabra: ");
+        String palabra2 = scanner.nextLine();
+
+        try {
+            verificarPalabras(palabra1, palabra2);
+            boolean riman = palabra1.substring(palabra1.length() - 2)
+                                    .equalsIgnoreCase(palabra2.substring(palabra2.length() - 2));
+            System.out.println((riman) ? "¡Estas palabras riman!" : "¡No riman!");
+
+        } catch (StringIndexOutOfBoundsException e) {
+            // Se lanza si alguna palabra tiene menos de 2 letras
+            System.out.println("¡Necesito al menos dos letras para rimar!");
+        } catch (IllegalArgumentException e) {
+            // Se lanza si alguna palabra contiene caracteres no válidos (números, símbolos, etc.)
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Método que verifica si ambas palabras contienen solo letras.
+     * Si alguna no lo cumple, lanza una excepción.
+     */
+    public static void verificarPalabras(String palabra1, String palabra2) throws IllegalArgumentException {
+        if (!palabra1.matches("^[a-zA-Z]+$") || !palabra2.matches("^[a-zA-Z]+$")) {
+            throw new IllegalArgumentException("¿Eso se considera una palabra?");
+        }
+    }
+}
